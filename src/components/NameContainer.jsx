e573313b843fb3f360dd7d4da5f2714c05eb3a43
Julian <juliiansandstrom@gmail.com>
2025-03-23 18:55:48 +0100

    logo update

--- src/components/NameContainer.jsx
+++ src/components/NameContainer.jsx
@@ -2,7 +2,7 @@ const NameContainer = ({ nameValue }) => {
   return (
     <div className="name-container ">
       {nameValue ? (
-        <h2 className="hello-name">Hello {nameValue}!</h2>
+        <h2 className="hello-name">Hi {nameValue}!</h2>
       ) : (
         <h2 className="hello-name">
           <span className="lag-color hello">Hello </span> Guest!
